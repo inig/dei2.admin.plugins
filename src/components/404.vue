eb0debfef04c2171c689a4a6c2992ae52490d4ae
梁山 <liangshan@192.168.0.106>
2017-12-06 00:03:57 +0800

    初始化项目

---
+++ src/components/404.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="not_found_container">
+        404 Not Found
+    </div>
+</template>
+<style>
+    .not_found_container {
+        width: 100%;
+        height: 100%;
+        background-color: #42b983;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+<script>
+  export default {
+    name: 'NotFound',
+    data () {
+      return {
+      }
+    },
+    components: {}
+  }
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login_container">
+        登录页面
+        <Button type="primary" @click="goTo">Go To 404</Button>
+    </div>
+</template>
+<style>
+    .login_container {
+        width: 100%;
+        height: 100%;
+        background-color: lightblue;
+    }
+</style>
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    methods: {
+      goTo () {
+        this.$router.push('/ss')
+      }
+    },
+    components: {}
+  }
+</script>
